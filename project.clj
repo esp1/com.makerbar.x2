3919da7338f2345036b307d41d01bf0a4c63b0d3
Edwin Park <esp@mecha1.com>
2013-09-06 15:36:08 -0400

    provide group id

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject x2 "0.1.0-SNAPSHOT"
+(defproject com.makerbar/x2 "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
