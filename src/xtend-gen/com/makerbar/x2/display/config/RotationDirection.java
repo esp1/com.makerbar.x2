dfcb8a9d8a43aa70cf83dad761fb50e2f103201b
Edwin Park <esp@mecha1.com>
2013-09-06 15:26:10 -0400

    ingore xtend-gen

--- src/xtend-gen/com/makerbar/x2/display/config/RotationDirection.java
+++
@@ -1,8 +0,0 @@
-package com.makerbar.x2.display.config;
-
-@SuppressWarnings("all")
-public enum RotationDirection {
-  COUNTERCLOCKWISE,
-  
-  CLOCKWISE;
-}
--- src/xtend-gen/com/makerbar/x2/display/config/SectorRowOrientation.java
+++
@@ -1,8 +0,0 @@
-package com.makerbar.x2.display.config;
-
-@SuppressWarnings("all")
-public enum SectorRowOrientation {
-  TOP_DOWN,
-  
-  BOTTOM_UP;
-}
