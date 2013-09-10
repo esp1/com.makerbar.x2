36e68227fb9923f4787556bbed77b117402ef953
Edwin Park <esp@mecha1.com>
2013-09-10 11:02:15 -0400

    return fps as double

--- src/clj/com/makerbar/x2/client.clj
+++ src/clj/com/makerbar/x2/client.clj
@@ -1,7 +1,7 @@
 (ns com.makerbar.x2.client
   (:gen-class
     :name com.makerbar.x2.X2Client
-    :methods [#^{:static true} [sendData [processing.core.PImage] long]])
+    :methods [#^{:static true} [sendData [processing.core.PImage] double]])
   (:require [aleph.tcp :refer [tcp-client]]
             [gloss.core :refer [defcodec header repeated]]
             [gloss.io :refer [decode encode]]
@@ -13,11 +13,11 @@
           (fn [b] \0)))  ; header = char \0
 
 (defcodec receive-codec
-  :uint64-le)
+  {:fps :float64-le})
 
 (defn x2-client
   "X2 client"
-  ([data] (x2-client "localhost" 10000 data))
+  ([data] (x2-client "10.0.0.200" 10000 data))
   ([host data] (x2-client host 10000 data))
   ([host port data]
     (let [ch (wait-for-result (tcp-client {:host host, :port port}))]
